778d3f9fcde270cfb84ca591606f693c3ef73e7d
reghis86 <lukasz.parfieniuk@gmail.com>
2018-09-17 14:55:06 +0200

    updated to v0.6
    Added:
    - Reading meta info of page,
    - Coverter to *.ashx and media library links
    - Css injects of various articles/snippets
    
    To do:
    - Draft paths easy transfer to xlsx

--- Sitecore CE tool/content.js
+++ Sitecore CE tool/content.js
@@ -1,34 +1,94 @@
 function getContentByMetaTagName(c) {
   for (var b = document.getElementsByTagName("meta"), a = 0; a < b.length; a++) {
-    if (c == b[a].name ) {
+    if (c == b[a].name) {
       return b[a].content;
-     }
-  } return false;
+    }
+  }
+  return false;
 }
+
+// draft-list
+/* var itemTitle = document.getElementsByClassName('scClickFont');
+var itemPath = document.getElementsByClassName('scFont');
+var itemTab = [];
+for ( var i = 0; i < itemTitle.length; i++ ) {
+  itemTab[i] = itemTitle[i].innerText + itemPath[i].innerText;
+}; */
 
 // Inform the background page that
 // this tab should have a page-action
 chrome.runtime.sendMessage({
-  from:    'content',
+  from: 'content',
   subject: 'showPageAction'
 });
 
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function (msg, sender, response) {
-  // First, validate the message's structure
+// Gather and send DOM info ( depends on the request ) to the popup.js
+chrome.runtime.onMessage.addListener(function(msg, sender, response) {
   if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
-    // Collect the necessary data
-    // (For your specific requirements `document.querySelectorAll(...)`
-    //  should be equivalent to jquery's `$(...)`)
-    var domInfo = {
-      itemid:   getContentByMetaTagName('itemId').substr(1, 36),
-      title:  getContentByMetaTagName('title'),
-      desc: getContentByMetaTagName('description'),
-      key: getContentByMetaTagName('keywords')
-    };
 
-    // Directly respond to the sender (popup),
-    // through the specified callback */
+    var domInfo = {
+      itemid: getContentByMetaTagName('itemId').substr(1, 36),
+      title: getContentByMetaTagName('title'),
+      desc: getContentByMetaTagName('description'),
+      key: getContentByMetaTagName('keywords'),
+      /* itemtab: itemTab */
+    };
     response(domInfo);
   }
+  if ((msg.from === 'button')) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    if (msg.subject === 'btnArtDt') {
+      link.href = chrome.extension.getURL("customCss/article_desktop.css");
+    }
+    if (msg.subject === 'btnArtMb') {
+      link.href = chrome.extension.getURL("customCss/article_mobile.css");
+    }
+    if (msg.subject === 'btnFaqDt') {
+      link.href = chrome.extension.getURL("customCss/faqRelatedVideos_desktop.css");
+    }
+    if (msg.subject === 'btnFaqMb') {
+      link.href = chrome.extension.getURL("customCss/faqRelatedVideos_mobile.css");
+    }
+    if (msg.subject === 'btnSsDt') {
+      link.href = chrome.extension.getURL("customCss/science_desktop.css");
+    }
+    if (msg.subject === 'btnSsMb') {
+      link.href = chrome.extension.getURL("customCss/science_mobile.css");
+    }
+    if (msg.subject === 'btnBgDt') {
+      link.href = chrome.extension.getURL("customCss/grooming_desktop.css");
+    }
+    if (msg.subject === 'btnBgMb') {
+      link.href = chrome.extension.getURL("customCss/grooming_mobile.css");
+    }
+    if (msg.subject === 'btnWodDt') {
+      link.href = chrome.extension.getURL("customCss/wet_or_dry_desktop.css");
+    }
+    if (msg.subject === 'btnWodMb') {
+      link.href = chrome.extension.getURL("customCss/wet_or_dry_mobile.css");
+    }
+    if (msg.subject === 'btnVenusDt') {
+      link.href = chrome.extension.getURL("customCss/venus_article_desktop.css");
+    }
+    if (msg.subject === 'btnVenusMb') {
+      link.href = chrome.extension.getURL("customCss/venus_article_mobile.css");
+    }
+    if (msg.subject === 'btnCustomDt') {
+      link.href = chrome.extension.getURL("customCss/custom_desktop.css");
+    }
+    if (msg.subject === 'btnCustomMb') {
+      link.href = chrome.extension.getURL("customCss/custom_mobile.css");
+    }
+    if (msg.subject === 'btnStylerDt') {
+      link.href = chrome.extension.getURL("customCss/styler_desktop.css");
+    }
+    if (msg.subject === 'btnStylerMb') {
+      link.href = chrome.extension.getURL("customCss/styler_mobile.css");
+    }
+    document.getElementsByTagName("head")[0].appendChild(link);
+    console.log('css from: ' + msg.subject + ' applied!');
+    response(msg.subject);
+  } 
 });
--- Sitecore CE tool/popup.js
+++ Sitecore CE tool/popup.js
@@ -1,31 +1,167 @@
 // Update the relevant fields with the new data
 function setDOMInfo(info) {
-  document.getElementById('itemid').textContent   = info.itemid;
-  document.getElementById('title').textContent  = info.title;
+  document.getElementById('itemid').textContent = info.itemid;
+  document.getElementById('title').textContent = info.title;
   document.getElementById('desc').textContent = info.desc;
   document.getElementById('key').textContent = info.key;
+  /* for (i = 0; i < info.itemtab.length; i++){
+    document.getElementById('draft-list').textContent += info.itemtab[i] + " \n";
+  }; */
 }
 
-// Once the DOM is ready...
-window.addEventListener('DOMContentLoaded', function () {
-  // ...query for the active tab...
+// Send message once DOM content is ready
+window.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({
     active: true,
     currentWindow: true
-  }, function (tabs) {
-    // ...and send a request for the DOM info...
+  }, function(tabs) {
     chrome.tabs.sendMessage(
-        tabs[0].id,
-        {from: 'popup', subject: 'DOMInfo'},
-        // ...also specifying a callback to be called
-        //    from the receiving end (content script)
-        setDOMInfo);
-        var textArea = document.getElementById('itemid');
-        var copyBtn1 = document.getElementById('copy1');
+      tabs[0].id, {
+        from: 'popup',
+        subject: 'DOMInfo'
+      },
+      setDOMInfo);
+    /* var textArea = document.getElementById('itemid');
+    var copyBtn1 = document.getElementById('copy1'); */
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('btn-ashx').addEventListener('click', convertToAshx);
+  document.getElementById('btn-to-title').addEventListener('click', convertToTitle);
+  document.getElementById('btn-show-more').addEventListener('click', toggleHidden);
+  document.getElementById('btnArtDt').addEventListener('click', applyCss, false);
+  document.getElementById('btnArtMb').addEventListener('click', applyCss, false);
+  document.getElementById('btnFaqDt').addEventListener('click', applyCss, false);
+  document.getElementById('btnFaqMb').addEventListener('click', applyCss, false);
+  document.getElementById('btnSsDt').addEventListener('click', applyCss, false);
+  document.getElementById('btnSsMb').addEventListener('click', applyCss, false);
+  document.getElementById('btnBgDt').addEventListener('click', applyCss, false);
+  document.getElementById('btnBgMb').addEventListener('click', applyCss, false);
+  document.getElementById('btnWodDt').addEventListener('click', applyCss, false);
+  document.getElementById('btnWodMb').addEventListener('click', applyCss, false);
+  document.getElementById('btnVenusDt').addEventListener('click', applyCss, false);
+  document.getElementById('btnVenusMb').addEventListener('click', applyCss, false);
+  document.getElementById('btnCustomDt').addEventListener('click', applyCss, false);
+  document.getElementById('btnCustomMb').addEventListener('click', applyCss, false);
+  document.getElementById('btnStylerDt').addEventListener('click', applyCss, false);
+  document.getElementById('btnStylerMb').addEventListener('click', applyCss, false);
+});
+
+
+// Sending button ID to content.js
+function applyCss(event) {
+  console.log(event.target.getAttribute('id'));
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id, {
+        from: 'button',
+        subject: event.target.getAttribute('id')
+      });
+  });
+};
+
+// Converter to title ( each word with upper case and remove "-" )
+function convertToTitle() {
+  var convertedText = document.getElementById('inputText').value;
+  if (convertedText) {
+    var str = convertedText.replace(/-/g, ' ').toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].split('');
+      str[i][0] = str[i][0].toUpperCase();
+      str[i] = str[i].join('');
+    }
+    return document.getElementById('outputText').textContent = str.join(' ');
+  }
+}
+
+// Converter to ashx hash ( upper case and remove "-" )
+function convertToAshx() {
+  var convertedText = document.getElementById('inputText').value;
+  if (convertedText[0] === '{') {
+    var str = convertedText.slice(1, -1).replace(/-/g, '').toUpperCase();
+  } else {
+    var str = convertedText.replace(/-/g, '').toUpperCase();
+  }
+  return document.getElementById('outputText').textContent = "-/media/" + str + ".ashx";
+}
+
+// Toggle more meta info
+function toggleHidden() {
+  var btn = document.getElementById('btn-show-more');
+  var content = document.getElementById('container-show-more');
+  btn.classList.toggle('hidden');
+  content.classList.toggle('hidden');
+}
+
+ // Toggable tabs script
+ // Listeners
+ document.addEventListener('DOMContentLoaded', function() {
+  var btnMeta = document.getElementById('btn-meta');
+  btnMeta.addEventListener('click', function() {
+      removeActive();
+      btnMeta.className += " is-active";
+      toggleTabs('meta-panel');
+  });
+
+  var btnConverter = document.getElementById('btn-converter');
+  btnConverter.addEventListener('click', function() {
+      removeActive();
+      btnConverter.className += " is-active";
+      toggleTabs('converter-panel');
+  });
+
+  var btnCss = document.getElementById('btn-css');
+  btnCss.addEventListener('click', function() {
+      removeActive();
+      btnCss.className += " is-active";
+      toggleTabs('css-panel');
+  });
+
+  var btnAbout = document.getElementById('btn-about');
+  btnAbout.addEventListener('click', function() {
+      removeActive();
+      btnAbout.className += " is-active";
+      toggleTabs('about-panel');
   });
 });
 
 
-if (copyBtn1) {
-    document.getElementById('copyBtn1').addEventListener('click', document.getElementById('itemid').select());
-};
+// Remove is-active class from buttons 
+function removeActive() {
+  var btnActive;
+
+  btnActive = document.getElementsByClassName('is-active');
+  for (i = 0; i < btnActive.length; i++) {
+      btnActive[i].classList.remove('is-active');
+  }
+}
+
+// Hide/show tabs 
+function toggleTabs(tabId) {
+  var tabContent;    
+
+  tabcontent = document.getElementsByClassName("main-tabs__tab");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].classList.remove('is-active');
+  }
+
+  document.getElementById(tabId).classList.add("is-active");
+}
+// Toggable panels script ends here
+
+// Meta info show more...
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('btn-show-more').addEventListener('click', toggleHidden);
+});
+
+function toggleHidden() {
+  var btn = document.getElementById('btn-show-more');
+  var content = document.getElementById('container-show-more');
+  btn.classList.toggle('hidden');
+  content.classList.toggle('hidden');
+}
+// Meta info show more... ends here
